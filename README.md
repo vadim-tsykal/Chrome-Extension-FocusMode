2f9d787943a6c34360daef3d7b02c7c2666c491b
vadim-tsykal <vadim.tsykal@outlook.com>
2023-01-14 16:06:36 +0100

    Added iFrame support, ver 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Chrome-Extension-FocusMode
-Hide annoying elements on a web page
+Customize any web page for your needs in a few clicks!
+
+A small and simple interactive tool for customising web pages. You decide what to hide by clicking on highlighted element as you move mouse pointer over the page. It's an efficient tool when you need to clean up a web page before printing or taking a screenshot, or when you need to focus on important content and remove distractors (heh, those autoplay videos, flashing banners and annoying popups). Now any web page can look like you want it in a few clicks! 
+
+How to use:
+- Install the extension
+- Activate the extension (click its icon) when you wish to adjust a web page
+- Move your mouse to the element you wish to hide
+- Click on selected element and watch it disappear
+- Deactivate the extension (click its icon again) to continue normal browsing
+
+A few facts about this piece of software:
+- It is free and open-source
+- It does not collect any information and does not interact with internet
+- It is not active while you are surfing and does not consume any resources unlike adblockers
+- It was inspired by a same-name Google example
+
+Enjoy!
+
+https://www.youtube.com/watch?v=FYfjMKdccWY
---
+++ focus-mode.js
@@ -0,0 +1,61 @@
+'use strict';
+
+function injectFocusMode()
+{
+  function lightUp(e)
+  {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+
+    e.target.classList.add('element-in-current-focus');
+  }
+
+  function lightOff(e)
+  {
+    e.preventDefault();
+    e.stopImmediatePropagation();    
+
+    e.target.classList.remove('element-in-current-focus');    
+  }
+
+  function hideMe(e)
+  {
+    e.preventDefault();
+    e.stopImmediatePropagation();    
+
+    e.target.classList.add('hide-element-in-focus-mode');
+    e.target.style.display = "none";
+
+    if (document.body === e.target)
+    {
+        chrome.runtime.sendMessage({ hideFrame: true })
+        .catch(err => chrome.runtime.lastError);
+    }
+  }
+
+  function eject()
+  {
+    document.body.removeEventListener("mouseover", lightUp);
+    document.body.removeEventListener("mouseout", lightOff);
+    document.body.removeEventListener("click", hideMe,true);    
+  }
+
+  document.body.addEventListener("mouseover", lightUp);
+  document.body.addEventListener("mouseout", lightOff);
+  document.body.addEventListener("click", hideMe,true);
+
+  window.ejectFocusMode = eject;
+}
+
+chrome.runtime.onMessage.addListener( request => 
+{
+    if (request.inject)
+    {
+      injectFocusMode();
+    } 
+    else if (request.eject && window.ejectFocusMode) 
+    {
+      ejectFocusMode();
+    }
+});
+
