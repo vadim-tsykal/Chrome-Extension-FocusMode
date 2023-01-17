4e513d75aafe4016be33321d0d98441887c854c3
vadim-tsykal <vadim.tsykal@outlook.com>
2023-01-17 15:48:40 +0100

    ver. 1.0.3

--- focus-mode.js
+++ focus-mode.js
@@ -1,5 +1,26 @@
 'use strict';
 
+chrome.runtime.onMessage.addListener( request => 
+{
+  const frame = window.top === window ? "main window" : "frame window";
+
+  if (request.inject)
+  {
+    log(`Inject request received in ${frame}`);
+    injectFocusMode();
+  } 
+  else if (request.eject && window.ejectFocusMode) 
+  {
+    log(`Eject request received in ${frame}`);
+    ejectFocusMode();
+  }
+
+  function log()
+  {
+    if (request.debugMode) notifyFocusMode({ message: [...arguments] });
+  }
+})
+
 function injectFocusMode()
 {
   function lightUp(e)
@@ -28,9 +49,7 @@ function injectFocusMode()
 
     if (document.body === e.target)
     {
-      chrome.runtime.sendMessage({ hideFrame: true })
-      .then( obj => void chrome.runtime.lastError)
-      .catch(err => void chrome.runtime.lastError);
+      notifyFocusMode({ hideFrame: true })
     }
   }
 
@@ -48,31 +67,10 @@ function injectFocusMode()
   window.ejectFocusMode = eject;
 }
 
-chrome.runtime.onMessage.addListener( request => 
+function notifyFocusMode(obj)
 {
-  const frame = window.top === window ? "main window" : "frame window";
-
-  if (request.inject)
-  {
-    log(`Inject request received in ${frame}`);
-    injectFocusMode();
-  } 
-  else if (request.eject && window.ejectFocusMode) 
-  {
-    log(`Eject request received in ${frame}`);
-    ejectFocusMode();
-  }
-
-  function log()
-  {
-    if (request.debugMode) notifyFocusMode(...arguments);
-  }
-})
-
-function notifyFocusMode()
-{
-  chrome.runtime.sendMessage({ message: [...arguments] })
-  .then( obj => void chrome.runtime.lastError)
+  chrome.runtime.sendMessage(obj)
+  .then( res => void chrome.runtime.lastError)
   .catch(err => void chrome.runtime.lastError);
 }
 
